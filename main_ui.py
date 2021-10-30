5aeb8131d7a6046216697cf4fbf0e4545ce58d13
egorshv22 <shvedov124@gmail.com>
2021-10-30 09:43:38 +0300

    Add form_ui.py

--- main_ui.py
+++ main_ui.py
@@ -14,7 +14,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(454, 359)
+        MainWindow.resize(1100, 700)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.horizontalLayout = QtWidgets.QHBoxLayout(self.centralwidget)
